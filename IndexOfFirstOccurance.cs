27eb317cb420fa8582be9e121a8d3c4f3448e5f4
ShilpashreeSrinivasamurthy <56570440+ShilpashreeSrinivasamurthy@users.noreply.github.com>
2023-09-23 22:13:05 +0530

    Create IndexOfFirstOccurance.cs

---
+++ IndexOfFirstOccurance.cs
@@ -0,0 +1,27 @@
+
+
+int IndexOfFirstOccurance(string haystack, string needle)
+        {
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (i + needle.Length > haystack.Length)
+                {
+                    break;
+                }
+                for (int j = 0; j < needle.Length; j++)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+                    if (j == needle.Length - 1)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        // Time Complexity :  O(m * n) where m = haystack length and n = needle length
+        // Space Complexity : O(1)
