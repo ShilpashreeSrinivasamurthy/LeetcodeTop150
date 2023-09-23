fb09758c627f08dab7c4553a12c18c6a8a48430e
ShilpashreeSrinivasamurthy <56570440+ShilpashreeSrinivasamurthy@users.noreply.github.com>
2023-09-23 11:58:39 +0530

    Create MergeSortedArrays.cs

---
+++ MergeSortedArrays.cs
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+void MergeSortedArrays(int[] nums1, int m, int [] nums2, int n)
+        {
+            int count1 = m - 1;
+            int count2 = n - 1;
+            int totalCount = m + n - 1;
+            while (count1 >= 0 && count2 >= 0)
+            {
+                nums1[totalCount--] = nums1[count1] >= nums2[count2] ? nums1[count1--] : nums2[count2--];
+            }
+            while (count2 >= 0)
+            {
+                nums1[totalCount--] = nums2[count2--];
+            }
+
+            foreach(int i in nums1)
+            {
+                Console.WriteLine(i);
+            }
+        }
